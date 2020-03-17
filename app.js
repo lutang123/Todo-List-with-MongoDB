72e83d272276b8699ae9ce42f4938e0e318d837c
Lu Tang <lutang@MacBook-Pro.hitronhub.home>
2020-03-17 13:48:35 -0700

    created databased

--- app.js
+++ app.js
@@ -15,23 +15,113 @@ app.use(bodyParser.urlencoded({extended: true}))
 // use the static method from express to access local file, because our css file is on our local
 app.use(express.static("public"))
 
-// const is JavaScript can be edited if it is an array or object
-const items = ["Eat well", "Do some exercise", "Sleep well"]
+
+// database
+const mongoose = require("mongoose")
+
+mongoose.connect('mongodb://localhost/todolistDB', {useNewUrlParser: true, useUnifiedTopology: true});
+
+const itemSchema = new mongoose.Schema({name: String})
+
+const ItemModel = mongoose.model("ItemCollection", itemSchema)
+
+const item1 = new ItemModel({name: "Eat well"})
+const item2 = new ItemModel({name: "Do some exercise"})
+const item3 = new ItemModel({name: "Sleep well"})
+
+const defaultItems  = [item1, item2, item3]
+
+const deletedSchema = new mongoose.Schema({name: String})
+
+const deletedModel = mongoose.model("deletedItem", deletedSchema)
+
+// from version 2
+// // const is JavaScript can be edited if it is an array or object
+// const items = ["Eat well", "Do some exercise", "Sleep well"]
+//
+// const dreamLists = ["Travel around the world", "Starting my own business", "Maybe go to space someday"]
 
 app.get("/", function(req, res) {
-  let day = date.getDate();
-  // in the ejs file, we have: <h1> <%= listTitle %> </h1> and <p><%= newListItems[i] %></p>
-  res.render("list", {listTitle: day, newListItems: items})
-  //before posting, it will show items as ["Eat well", "Do some exercise", "Sleep well"]
+
+  let day = date.getDate();  //date refers to date.js that we have required, and getDate() is the function we defined.
+
+  ItemModel.find({}, function(err, fruits) {  // fruits can be any word and it refers to the collections
+
+    if (fruits.length === 0) {
+
+      ItemModel.insertMany(defaultItems, function(err) {
+        if (err) {
+          console.log(err)
+        } else {
+          console.log("Successfully saved default items to DB")
+        }
+      })
+      res.redirect("/") // if i don't write this, it's still working, why??
+    }
+      else {
+        res.render("list", {listTitle: day, newListItems: fruits})
+    }
+  })
 })
+  // in the ejs file, we have: <h1> <%= listTitle %> </h1> and <p><%= newListItems[i] %></p>
+  // res.render("list", {listTitle: day, newListItems: items})
+  //before posting, it will show items as ["Eat well", "Do some exercise", "Sleep well"]
 
 app.post("/", function(req, res) {
-  let item = req.body.newItem
-  items.push(item)
+  const newItem = new ItemModel({name: req.body.newItem})
+  if (newItem.length === 0) {
+    res.redirect("/")
+  } else {
+    newItem.save()
+    res.redirect("/")
+  }
+})
+
+app.post("/completed", function(req, res) {
+
   console.log(req.body)
-  console.log(req.body.button)
-  res.redirect("/")
+  console.log(req.body.deletedItem)
+
+  const deletedItem = new deletedModel({name: req.body.deletedItem})
+  if (deletedItem.length === 0) {
+    deletedModel.deleteOne({name: deletedItem}, function(err) {
+      if (err) {
+        console.log(err)
+      } else {
+        console.log("Successfully deleted empty string")
+      }
+   })
+  } else {
+    deletedItem.save()
+  }
+
+  ItemModel.deleteOne({name: req.body.deletedItem}, function(err) {
+    if (err) {
+      console.log(err)
+    } else {
+      console.log("Successfully deleted the document")
+    }
+    ItemModel.find({}, function(err, fruits) {
+      if (fruits.length === 0) {
+        res.redirect("/completed")
+      } else {
+        res.redirect("/")
+      }
+    })
   })
+})
+
+app.get("/completed", function(req, res) {
+  deletedModel.find({}, function(err, fruits) {  // fruits can be any word and it refers to the collections
+    res.render("completed", {listTitle: "Congratulations!", deletedItems: fruits})
+  })
+})
+  // ItemModel.findByIdAndRemove(checkedItemId, function(err) {
+  //   if(!err) {
+  //     console.log("Successfully deleted the document")
+  //   }
+  // })
+  // res.redirect("/")
 
 
 const dreamLists = ["Travel around the world", "Starting my own business", "Maybe go to space someday"]
